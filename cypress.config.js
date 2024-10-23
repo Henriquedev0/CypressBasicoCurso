df90eefc7706a94f928364f03b2ecd30575bb071
Caio Henrique <124810959+Henriquedev0@users.noreply.github.com>
2024-10-23 14:11:51 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'Aprendizado e2e',
   e2e: {
-    baseUrl: 'http://localhost:5500/cypress/src',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
